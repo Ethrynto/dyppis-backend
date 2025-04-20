69d1bf798d45a08881f3f4e1a68d2500010769d6
Ethrynto <ethrynto@gmail.com>
2025-04-20 17:47:34 +0300

    Realize model

--- dyppis_backend/app/Models/ProductService/Category.php
+++ dyppis_backend/app/Models/ProductService/Category.php
@@ -2,9 +2,46 @@
 
 namespace App\Models\ProductService;
 
+use App\Models\MediaService\Mediafile;
+use App\Models\Translation;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Category extends Model
 {
-    //
+    protected $table = 'categories';
+
+    protected $keyType = 'string';
+    public $incrementing = false;
+    protected $fillable = [
+        'id',
+        'slug',
+        'title_id',
+        'logo_id',
+        'is_public',
+    ];
+
+    protected $casts = [
+        'id' => 'string',
+        'slug' => 'string',
+        'title_id' => 'string',
+        'logo_id' => 'string',
+        'is_public' => 'boolean',
+    ];
+
+    /**
+     *  Category title.
+     */
+    public function title(): BelongsTo
+    {
+        return $this->belongsTo(Translation::class, 'title_id', 'id');
+    }
+
+    /**
+     *  Category logo.
+     */
+    public function logo(): BelongsTo
+    {
+        return $this->belongsTo(Mediafile::class, 'logo_id', 'id');
+    }
 }
