96a34b45d77e3eacba2aca0e9d2890c424b69a3a
Ethrynto <ethrynto@gmail.com>
2025-04-20 17:50:06 +0300

    Added the auth routes, refactored the platform routes

--- dyppis_backend/routes/api.php
+++ dyppis_backend/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Api\V1\ProductService\PlatformController;
 use App\Http\Controllers\Api\V1\UserService\UserController;
+use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
 
@@ -16,9 +17,33 @@ Route::prefix('v1')->group(function () {
 
     Route::get('/platform-categories', [\App\Http\Controllers\Api\V1\ProductService\PlatformCategoryController::class, 'index']);
 
+    Route::prefix('auth')->group(function () {
+        Route::post('/authorization', [AuthController::class, 'authorization'])   // User authorization
+            ->name('authorization');
+        Route::post('/registration', [AuthController::class, 'registration'])     // User registration
+            ->name('registration');
+        Route::post('/logout', [AuthController::class, 'logout'])                 // User logout
+            ->middleware('auth:sanctum')
+            ->name('logout');
+    });
+
+    /*
+     *  Platform CRUD operations
+     */
     Route::prefix('platforms')->group(function () {
-        Route::get('/', [PlatformController::class, 'index']);      // Get all platforms
-        Route::get('/{id}', [PlatformController::class, 'show']);   // Get a platform by ID
-        Route::post('/', [PlatformController::class, 'store']);     // Create a new platform
+
+        Route::get('/', [PlatformController::class, 'index'])               // Get all platforms
+            ->name('platforms.index');
+        Route::get('/{id}', [PlatformController::class, 'show'])            // Get a platform by ID
+            ->name('platforms.show');
+        Route::post('/', [PlatformController::class, 'store'])              // Create a new platform
+            ->name('platforms.store')
+            ->middleware('auth:sanctum');
+        Route::patch('/{id}', [PlatformController::class, 'update'])         // Update a platform by ID
+            ->name('platforms.update')
+            ->middleware('auth:sanctum');
+        Route::delete('/{id}', [PlatformController::class, 'destroy'])      // Remove a platform by ID
+            ->name('platforms.destroy');
+
     });
 });
