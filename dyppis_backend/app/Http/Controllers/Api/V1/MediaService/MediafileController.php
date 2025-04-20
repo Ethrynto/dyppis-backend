5a5ba334c2058e52ba543275d58012c448682fe6
Ethrynto <ethrynto@gmail.com>
2025-04-20 09:29:26 +0300

    Refactor the methods for load and remove files

--- dyppis_backend/app/Http/Controllers/Api/V1/MediaService/MediafileController.php
+++ dyppis_backend/app/Http/Controllers/Api/V1/MediaService/MediafileController.php
@@ -3,27 +3,73 @@
 namespace App\Http\Controllers\Api\V1\MediaService;
 
 use App\Http\Controllers\Controller;
-use App\Http\Requests\MediaService\StoreMediafileRequest;
 use App\Models\MediaService\Mediafile;
 use App\Models\MediaService\MediafileCategory;
+use App\Utils\ApiResponse;
+use App\Utils\UuidHelper;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
+use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 
 class MediafileController extends Controller
 {
-    public function store(StoreMediafileRequest $request)
+    /**
+     *  This method is used to load a picture file into the storage.
+     *
+     *  @param mixed $imageFile Picture file
+     *  @param string|null $categoryId The category id or slug for load picture
+     *  @return mixed
+     *  @throws BadRequestException
+     */
+    public static function load(mixed $imageFile, string|null $categoryId = null): mixed
     {
-        $file = $request->file('file');
-        $mediafileCategory = MediafileCategory::where('id', $request->input('category_id'))->firstOrFail();
-        $fileName = Str::uuid() . '.' . $file->getClientOriginalExtension();
-        $filePath = $file->storeAs($mediafileCategory->path, $fileName, 'public');
+        $category = null;
+        if (UuidHelper::isUuid($categoryId) && $categoryId !== null)
+            $category = MediafileCategory::findOrFail($categoryId);
+        else
+            $category = MediafileCategory::where('slug', $categoryId ?? 'product')
+                ->firstOrFail();
 
-        $mediafile = Mediafile::create([
+        $fileInfo = [
             'id' => (string) Str::uuid(),
-            'file_name' => $fileName,
-            'file_path' => $filePath,
-            'category_id' => $request->input('category_id'),
-        ]);
+            'file_name' => Str::uuid() . '.' . $imageFile->getClientOriginalExtension(),
+            'file_type' => $imageFile->getMimeType(),
+            'file_size' => $imageFile->getSize(),
+            'category_id' => $category->id,
+            'created_at' => now(),
+        ];
 
-        return response()->json(['id' => $mediafile->id], 201);
+        try {
+            Storage::disk('public')->putFileAs($category->path, $imageFile, $fileInfo['file_name']);
+            Mediafile::insert($fileInfo);
+            return ApiResponse::created($fileInfo);
+        }
+        catch (\Exception $e) {
+            $errorInfo = [
+                'details' => $e->getMessage(),
+                'code' => $e->getCode(),
+            ];
+            throw new BadRequestException($errorInfo);
+        }
+    }
+
+    /**
+     *  Remove the specified resource from storage.
+     *
+     *  @param string $id
+     *  @return JsonResponse
+     */
+    public static function remove(string $id): JsonResponse
+    {
+        $fileInfo = Mediafile::where('id', $id)
+            ->first(['file_name', 'category_id']);
+        $categoryPath = MediafileCategory::where('id', $fileInfo->category_id)
+            ->pluck('path')
+            ->first();
+        Storage::disk('public')->delete($categoryPath . '/' . $fileInfo->file_name);
+        Mediafile::destroy($id);
+
+        return ApiResponse::deleted();
     }
 }
