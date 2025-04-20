d4076e8a441bd97b92bdf2d8fb9f3fce42899086
Ethrynto <ethrynto@gmail.com>
2025-04-20 17:49:00 +0300

    Added a new field

--- dyppis_backend/app/Http/Resources/ProductService/PlatformResource.php
+++ dyppis_backend/app/Http/Resources/ProductService/PlatformResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources\ProductService;
 
 use App\Http\Resources\MediaService\MediafileResource;
 use App\Http\Resources\TranslationResource;
+use App\Models\ProductService\Category;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -21,6 +22,7 @@ class PlatformResource extends JsonResource
             'title' => ($this->title),
             'logo' => MediafileResource::make($this->logo) ?? null,
             'category' => PlatformCategoryResource::make($this->category),
+            'categories' => CategoryResource::collection($this->categories),
             'parent' => PlatformResource::make($this->parent) ?? null,
             'sales' => $this->sales,
             'views' => $this->views,
